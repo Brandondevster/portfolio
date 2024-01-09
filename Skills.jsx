d11bef899a80733c0025724ba22c7131e415883f
Brandon Webster <92768663+Brandondevster@users.noreply.github.com>
2024-01-08 23:47:14 -0500

    Add files via upload

---
+++ Skills.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Node from "../assets/node.png";
+import Giticon from "../assets/giticon.png";
+import Python from "../assets/python.png";
+import Django from "../assets/django.png";
+import Vscode from "../assets/vscode.png";
+import Github from "../assets/github.png";
+import Tailwind from "../assets/tailwind.png";
+import jQuery from "../assets/jQuery.png"
+
+const Skills = () => {
+  return (
+    <div name="skills" className="w-full h-screen bg-[#000000] text-[#eeeeee]">
+      {/* Container*/}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center text-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline text-[#ffd600]">Skills</p>
+          <p className="text-lg text-[#eeeeee] py-4">
+            These are the technologies I build with.
+          </p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-6 text-center py-8">
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Giticon} alt="GIT icon" />
+            <p className="my-4">Git</p>
+          </div>
+          
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="JAVASCRIPT icon" />
+            <p className="my-4">JavaScript</p>
+          </div>
+          
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto"src={ReactImg}alt="REACT icon"/>
+            <p className="my-4">React</p>
+          </div>
+          
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Node} alt="NODE icon" />
+            <p className="my-4">Node</p>
+          </div>
+          
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={jQuery} alt="JQUERY icon" />
+            <p className="my-4">jQuery</p>
+          </div>
+
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Python} alt="PYTHON icon" />
+            <p className="my-4">Python</p>
+          </div>
+          
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Django} alt="DJANGO icon" />
+            <p className="my-4">Django</p>
+          </div>
+          
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="TAILWIND icon" />
+            <p className="my-4">Tailwind</p>
+          </div>
+          
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Vscode} alt="VSCODE icon" />
+            <p className="my-4">VS Code</p>
+          </div>
+
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Github} alt="GITHUB icon" />
+            <p className="my-4">GitHub</p>
+          </div>
+
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p className="my-4">HTML</p>
+          </div>
+
+          <div className="shadow-md shadow-[#3a3a3e] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
+            <p className="my-4">CSS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
--- Work.jsx
+++ Work.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import pythontetris from "../assets/pythontetris.png";
 import notepad from "../assets/notepad.png";
 import techmart from "../assets/techmart.png";
+import portfolio from "../assets/portfolio.png";
 import fashionpet from "../assets/fashionpet.png";
 import warnerwoodworks from "../assets/warnerwoodworks.png";
-import portfolio from "../assets/portfolio.png"
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-full text-[#c2c2c2] bg-[#000000]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+    <div name="work" className="w-full md:h-screen text-[#c2c2c2] bg-[#000000]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center text-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline text-[#c9b953]"><center>Work</center></p>
-          <p className="text-lg text-[#dddddd] py-6">
-            <center>Have a look at my recent projects.</center>
+          <p className="text-4xl font-bold inline text-[#ffd600]">Work</p>
+          <p className="text-lg text-[#eeeeee] py-6">
+            Have a look at my recent projects.
           </p>
         </div>
 
@@ -30,26 +30,13 @@ const Work = () => {
                 Warner Woodworks
               </span>
               <div className="pt-8 text-center">
-                <a href="https://warnerwoodworkssd.com/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#dddddd] font-bold text-lg">
+                <a href="https://tech-mart-site.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
                     Live
                   </button>
                 </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${portfolio})` }}
-            className="shadow-lg shadow-[#3a3a3e] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                React Portfolio
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://github.com/Brandondevster/portfolio">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#dddddd] font-bold text-lg">
+                <a href="https://github.com/Brandondevster/techmart">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -58,22 +45,17 @@ const Work = () => {
           </div>
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${fashionpet})` }}
+            style={{ backgroundImage: `url(${portfolio})` }}
             className="shadow-lg shadow-[#3a3a3e] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-black tracking-wider">
-                JS Pet Registration
+                React+Tailwind Portfolio
               </span>
               <div className="pt-8 text-center">
-                <a href="https://fashion-pet.netlify.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#dddddd] font-bold text-lg">
-                    Live
-                  </button>
-                </a>
-                <a href="https://github.com/Brandondevster/Fashion_Pet">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#dddddd] font-bold text-lg">
+                <a href="https://github.com/Brandondevster/portfolio">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -92,18 +74,43 @@ const Work = () => {
               </span>
               <div className="pt-8 text-center">
                 <a href="https://tech-mart-site.netlify.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#dddddd] font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
                     Live
                   </button>
                 </a>
                 <a href="https://github.com/Brandondevster/techmart">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#dddddd] font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
                     Code
                   </button>
                 </a>
               </div>
             </div>
           </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${fashionpet})` }}
+            className="shadow-lg shadow-[#3a3a3e] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                JS Pet Registration
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://fashion-pet.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+                <a href="https://github.com/Brandondevster/Fashion_Pet">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${notepad})` }}
             className="shadow-lg shadow-[#3a3a3e] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -111,11 +118,11 @@ const Work = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-black tracking-wider">
-                React+Django NotePad
+                React+Django Note Pad
               </span>
               <div className="pt-8 text-center">
                 <a href="https://github.com/Brandondevster/notepad">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#dddddd] font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
                     Code
                   </button>
                 </a>
@@ -134,7 +141,7 @@ const Work = () => {
               </span>
               <div className="pt-8 text-center">
                 <a href="https://github.com/Brandondevster/python_tetris">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#dddddd] font-bold text-lg">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-black text-[#eeeeee] font-bold text-lg">
                     Code
                   </button>
                 </a>
