4d72a574d8c6bcc9e27f71168999bf67d6116c40
Brandon <brandonkw13333@gmail.com>
2025-03-04 22:15:01 -0500

    adjusted sizing of skills.jsx & work.jsx for mobile view to fit correctly

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,7 +15,7 @@ import jQuery from "../assets/jQuery.png"
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#000000] text-[#eeeeee]">
+    <div name="skills" className="w-full md:h-screen bg-[#000000] text-[#eeeeee]">
       {/* Container*/}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center text-center w-full h-full">
         <div>
